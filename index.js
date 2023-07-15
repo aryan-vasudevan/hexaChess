10d159918c9da586f3c0632f6f03659c49e2c6b6
aryan-vasudevan <vasudevan.aryan@gmail.com>
2023-07-15 14:39:34 -0400

    Displayed tiles. Need to put them in hexagonal shape

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function Bishop (pos) {
     this.pos = "f12"
 }
 
-var boardMap = [
+const boardMap = [
  ['', '', '', '', '', 't', '', '', '', '', ''],
  ['', '', '', '', 't', '', 't', '', '', '', ''],
  ['', '', '', 't', '', 't', '', 't', '', '', ''],
@@ -27,14 +27,20 @@ var boardMap = [
 ];
 
 for (var i = 0; i < 21; i++) {
-    var row = boardMap[i]
-    var row_object = document.getElementById("row-" + i);
+    var row = boardMap[i];
+    var row_object = document.createElement("div");
+    row_object.setAttribute("id", "row-" + i);
+    row_object.style.width = "100%";
+    document.getElementById("board").appendChild(row_object);
 
     for (var j = 0; j < 11; j++) {
         if (row[j] === '') {
-            console.log("space")
+            console.log("space");
         } else {
-            console.log("tile")
+            var tile = document.createElement("img");
+            tile.setAttribute("class", "tile")
+            document.getElementById("row-" + i).appendChild(tile)
+            tile.src = "img/tile.png";
         }
     }
 }
