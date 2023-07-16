c62a5259400e189e58a67c5559f5fb65fcaeaf31
aryan-vasudevan <vasudevan.aryan@gmail.com>
2023-07-16 19:26:26 -0400

    Displayed tiles in hexagonal shape. Made board

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-function Bishop (pos) {
-    this.pos = "f12"
-}
-
 const boardMap = [
  ['', '', '', '', '', 't', '', '', '', '', ''],
  ['', '', '', '', 't', '', 't', '', '', '', ''],
@@ -29,17 +25,16 @@ const boardMap = [
 for (var i = 0; i < 21; i++) {
     var row = boardMap[i];
     var row_object = document.createElement("div");
-    row_object.setAttribute("id", "row-" + i);
-    row_object.style.width = "100%";
+    row_object.setAttribute("id", "row-" + (i + 1));
+    row_object.setAttribute("class", "row");
+
     document.getElementById("board").appendChild(row_object);
 
     for (var j = 0; j < 11; j++) {
-        if (row[j] === '') {
-            console.log("space");
-        } else {
-            var tile = document.createElement("img");
-            tile.setAttribute("class", "tile")
-            document.getElementById("row-" + i).appendChild(tile)
+        var tile = document.createElement("img");
+        tile.setAttribute("class", "tile");
+        if (row[j] != '') {
+            document.getElementById("row-" + (i + 1)).appendChild(tile);
             tile.src = "img/tile.png";
         }
     }
