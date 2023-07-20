7774a29b2b8790b94d05a2f28825277cc5ec4f6b
aryan-vasudevan <vasudevan.aryan@gmail.com>
2023-07-20 14:49:08 -0400

    Added turn based movement. Cleaned up code

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 // INITIAL START 
 //
-//
 // Map of the Board (Constant)
 const boardMap = [
     ['', '', '', '', '', 't', '', '', '', '', ''],
@@ -30,55 +29,55 @@ const boardMap = [
 const pieceInfo = {
 // White Pieces
    // Bishop
-    'bishop-1-w': ['bishop-w.png', '1-1' ],
-    'bishop-2-w': ['bishop-w.png', '3-2' ],
-    'bishop-3-w': ['bishop-w.png', '5-3' ],
+    'bishop-1-w': ['bishop-w.png', '1-1'],
+    'bishop-2-w': ['bishop-w.png', '3-2'],
+    'bishop-3-w': ['bishop-w.png', '5-3'],
     // Queen
-    'queen-w': ['queen-w.png', '2-1' ],
+    'queen-w': ['queen-w.png', '2-1'],
     // King
-    'king-w': ['king-w.png', '2-2' ],
+    'king-w': ['king-w.png', '2-2'],
     // Knight
     'knight-1-w': ['knight-w.png', '3-1'],
-    'knight-2-w': ['knight-w.png', '3-3' ],
+    'knight-2-w': ['knight-w.png', '3-3'],
     // Rook
-    'rook-1-w': ['rook-w.png', '4-1' ],
-    'rook-2-w': ['rook-w.png', '4-4' ],
+    'rook-1-w': ['rook-w.png', '4-1'],
+    'rook-2-w': ['rook-w.png', '4-4'],
     // Pawn
-    'pawn-1-w': ['pawn-w.png', '5-1' ],
-    'pawn-2-w': ['pawn-w.png', '6-2' ],
-    'pawn-3-w': ['pawn-w.png', '7-2' ],
-    'pawn-4-w': ['pawn-w.png', '8-3' ],
-    'pawn-5-w': ['pawn-w.png', '9-3' ],
-    'pawn-6-w': ['pawn-w.png', '8-4' ],
-    'pawn-7-w': ['pawn-w.png', '7-4' ],
-    'pawn-8-w': ['pawn-w.png', '6-5' ],
-    'pawn-9-w': ['pawn-w.png', '5-5' ],
+    'pawn-1-w': ['pawn-w.png', '5-1'],
+    'pawn-2-w': ['pawn-w.png', '6-2'],
+    'pawn-3-w': ['pawn-w.png', '7-2'],
+    'pawn-4-w': ['pawn-w.png', '8-3'],
+    'pawn-5-w': ['pawn-w.png', '9-3'],
+    'pawn-6-w': ['pawn-w.png', '8-4'],
+    'pawn-7-w': ['pawn-w.png', '7-4'],
+    'pawn-8-w': ['pawn-w.png', '6-5'],
+    'pawn-9-w': ['pawn-w.png', '5-5'],
 
 // Black Pieces
     // Bishop
-    'bishop-1-b': ['bishop-b.png',"21-1"],
-    'bishop-2-b': ['bishop-b.png',"19-2"],
-    'bishop-3-b': ['bishop-b.png',"17-3"],
+    'bishop-1-b': ['bishop-b.png','21-1'],
+    'bishop-2-b': ['bishop-b.png','19-2'],
+    'bishop-3-b': ['bishop-b.png','17-3'],
     // Queen
-    'queen-b': ['queen-b.png',"20-1"],
+    'queen-b': ['queen-b.png','20-1'],
     // King
-    'king-b': ['king-b.png', "20-2"],
+    'king-b': ['king-b.png', '20-2'],
     // Knight
-    'knight-1-b': ['knight-b.png',"19-1"],
-    'knight-2-b': ['knight-b.png',"19-3"],
+    'knight-1-b': ['knight-b.png','19-1'],
+    'knight-2-b': ['knight-b.png','19-3'],
     // Rook
-    'rook-1-b': ['rook-b.png', "18-1"],
-    'rook-2-b': ['rook-b.png',  "18-4"],
+    'rook-1-b': ['rook-b.png', '18-1'],
+    'rook-2-b': ['rook-b.png',  '18-4'],
     // Pawn
-    'pawn-1-b': ['pawn-b.png', "17-1"],
-    'pawn-2-b': ['pawn-b.png', "16-2"],
-    'pawn-3-b': ['pawn-b.png', "15-2"],
-    'pawn-4-b': ['pawn-b.png', "14-3"],
-    'pawn-5-b': ['pawn-b.png', "13-3"],
-    'pawn-6-b': ['pawn-b.png', "14-4"],
-    'pawn-7-b': ['pawn-b.png', "15-4"],
-    'pawn-8-b': ['pawn-b.png', "16-5"],
-    'pawn-9-b': ['pawn-b.png', "17-5"]
+    'pawn-1-b': ['pawn-b.png', '17-1'],
+    'pawn-2-b': ['pawn-b.png', '16-2'],
+    'pawn-3-b': ['pawn-b.png', '15-2'],
+    'pawn-4-b': ['pawn-b.png', '14-3'],
+    'pawn-5-b': ['pawn-b.png', '13-3'],
+    'pawn-6-b': ['pawn-b.png', '14-4'],
+    'pawn-7-b': ['pawn-b.png', '15-4'],
+    'pawn-8-b': ['pawn-b.png', '16-5'],
+    'pawn-9-b': ['pawn-b.png', '17-5']
 };
 
 // Tile
@@ -91,14 +90,15 @@ class Tile {
         this.tileSrc = tileSrc;
     }
 
-    // Method for loading tile
     createTile() {
+        // Create the container for both the tile and piece
         var container = document.createElement("div");
         container.setAttribute("id", this.containerId);
         container.setAttribute("class", "container");
         container.addEventListener("click", handleclick);
         document.getElementById(this.row).appendChild(container);
 
+        // Load the tile in the container
         var tile = document.createElement("img");
         tile.setAttribute("id", this.tileId);
         tile.setAttribute("class", "tile");
@@ -107,6 +107,7 @@ class Tile {
     }  
 }   
 
+// Piece
 class Piece {
     constructor(name) {
         this.name = name;
@@ -114,10 +115,11 @@ class Piece {
         this.id = `${name}`;
     }
 
-    // Method for loading a piece
     loadPeice() {
+        // Getting the container that was created when the tiles were being made
         parent = document.getElementById(`${this.pos}`);
 
+        // Create the piece
         var piece = document.createElement("img");
         piece.src = `img/pieces/${pieceInfo[this.name][0]}`;
         piece.setAttribute("id", this.id);
@@ -126,7 +128,6 @@ class Piece {
     }
 }
 
-// Function for board setup
 function setBoard() {
     for (var i = 1; i <= 21; i++) {
         // Create the rows
@@ -135,7 +136,7 @@ function setBoard() {
         row_object.setAttribute("class", "row");
         document.getElementById("board").appendChild(row_object);
 
-        // Create the tiles and pieces
+        // Create the tiles
         var column = 0;
         var row = i;
         var tileSrc = `tile${i % 3}.png`;
@@ -150,39 +151,49 @@ function setBoard() {
     }    
 }
 
-// Function for setting peices at the begining
 function setPeices() {
+    // Iterate through the pieces and call the method to load a piece
     for (var name of Object.keys(pieceInfo)) {
         var piece = new Piece(name);
         piece.loadPeice();
     }
 }
 
-// Function for updating a piece position if it needs to be moved
-function updatePeice(newPos, pieceId) {
+function updatePiece(newPos, pieceId) {
+    // Update the piece position by getting the new position (tile clicked) and move it there
     parent = document.getElementById(newPos);
     parent.appendChild(document.getElementById(pieceId));
 }
 
-
-// What should happend when something is clicked
-var held = [];
+// Piece currently held
+var held = '';
+var turn = 'w'
 
 function handleclick() {
+    // If the thing clicked was a piece 
     if (this.children.length === 2) {
-        held = [this.children[1].id, "p"];
+        held = this.children[1].id;
     
-    // Check if a tile was clicked. If a tile was clicked and the last thing clicked was a piece, move the piece to the new tile
-    } else if (held.length > 1 && held[1] === "p") {
-        var pieceId = held[0];
-        var pieceSrc = pieceInfo[pieceId][0];
-
+    // If the thing clicked was a tile and a piece is being held
+    // Check if the piece is in accordance with whose turn it is
+    } else if (held != '' && held.slice(-1) === turn) {
         // Update the Piece Position
-        pieceInfo[pieceId] = [pieceSrc, this.id];
-        updatePeice(this.id, held[0])
-        held = []; 
+        var pieceSrc = pieceInfo[held][0];
+        pieceInfo[held] = [pieceSrc, this.id];
+        updatePiece(this.id, held);
+        
+        // After the piece moves, nothing is held
+        held = '';
+
+        // Change Turns
+        if (turn === 'w') {
+            turn = 'b';
+        } else {
+            turn = 'w';
+        }
     }
 }
 
+// Start the game
 setBoard();
 setPeices();
